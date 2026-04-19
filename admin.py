5a03df22f6d35e8b8e80ca8cff2ee368c0d656fa
Hansika Gupta <hansikag22@gmail.com>
2026-04-19 17:28:02 +0530

    Delete admin.py

--- admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import ContentItem
-
-@admin.register(ContentItem)
-class ContentItemAdmin(admin.ModelAdmin):
-    list_display = ['title', 'user', 'category', 'status', 'rating', 'progress', 'updated_at']
-    list_filter = ['category', 'status', 'rating']
-    search_fields = ['title', 'user__username']
